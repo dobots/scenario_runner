fa034779a39fc87e3ca2bb29987bc32ab2b94b52
Reka-Berci-Hajnovics <reka.hajnovics@gmail.com>
2021-12-01 15:18:05 +0100

    bt_demo

---
+++ bt_demo/src/batterycheck.cpp
@@ -0,0 +1,46 @@
+//Include the behaviortree
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+#include "rclcpp/rclcpp.hpp"
+
+using namespace BT;
+
+// Simple function that return a NodeStatus
+BT::NodeStatus CheckBattery()
+{
+    std::cout << "[ Battery: OK ]" << std::endl;
+    return BT::NodeStatus::SUCCESS;
+}
+
+
+int main(int argc, char * argv[])
+{
+
+    rclcpp::init(argc, argv);
+ 
+  // We use the BehaviorTreeFactory to register our custom nodes
+    BehaviorTreeFactory factory;
+
+ 
+    // Registering a SimpleActionNode using a function pointer.
+    // you may also use C++11 lambdas instead of std::bind
+    factory.registerSimpleCondition("CheckBattery", std::bind(CheckBattery));
+        
+    // Trees are created at deployment-time (i.e. at run-time, but only 
+    // once at the beginning). 
+
+    // IMPORTANT: when the object "tree" goes out of scope, all the 
+    // TreeNodes are destroyed
+    auto tree = factory.createTreeFromFile("/home/reka/foxy2_ws/src/bt_demo/my_tree.xml");
+    
+    printTreeRecursively(tree.rootNode());
+
+    // To "execute" a Tree you need to "tick" it.
+    // The tick is propagated to the children based on the logic of the tree.
+    // In this case, the entire sequence is executed, because all the children
+    // of the Sequence return SUCCESS.
+    tree.tickRoot();
+  
+  rclcpp::shutdown();
+  return 0;
+}
