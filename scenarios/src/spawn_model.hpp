e178a22fdaec353013160ff211b4b40c9273a724
Reka-Berci-Hajnovics <reka.hajnovics@gmail.com>
2022-03-11 13:45:21 +0100

    modified:   scenarios/src/spawn_model.hpp

--- scenarios/src/spawn_model.hpp
+++ scenarios/src/spawn_model.hpp
@@ -23,20 +23,24 @@
 
 const int MAX_RANGE=25;
 
+using namespace BT;
+using namespace std;
+
+
 //opens the file and the file_path and stores it content in the xml_desc variable
-auto readFile(std::shared_ptr<rclcpp::Node> node, std::string file_path){
-   std::string xml_desc;
+auto readFile(shared_ptr<rclcpp::Node> node, string file_path){
+   string xml_desc;
 
    RCLCPP_INFO(node->get_logger(), "Loading entity XML from file: %s",file_path.c_str());
    
    //open the file
-   std::ifstream ifile;
+   ifstream ifile;
          ifile.open(file_path);
          if(!ifile){
             RCLCPP_INFO(node->get_logger(),"Error: specified file %s does not exist", file_path.c_str());
          }else{
             while ( !ifile.eof() ) { // keep reading until end-of-file
-            	std::string line;
+            	string line;
             	ifile >> line;
             	xml_desc += line;
             	xml_desc += " ";	
@@ -49,58 +53,21 @@ auto readFile(std::shared_ptr<rclcpp::Node> node, std::string file_path){
          pugi::xml_parse_result res = doc.load_string(xml_desc.c_str());
          if(!res)
          {
-          RCLCPP_INFO(node->get_logger(),"Invalid XML: %s", std::string(res.description()).c_str());
+          RCLCPP_INFO(node->get_logger(),"Invalid XML: %s", string(res.description()).c_str());
           }
           
      return xml_desc;          
   }
 
 
-/*auto calculatePose(){
 
-        geometry_msgs::msg::Pose initial_pose;
-        //Optional x,y,z, R,P,Y
-        std::map<std::string, float> received_pose;
-        received_pose["x"] = 0;
-        received_pose["y"] = 0;
-        received_pose["z"] = 0;
-        received_pose["R"] = 0;
-        received_pose["P"] = 0;
-        received_pose["Y"] = 0;
-        
-        std::string value;
-        for(auto item: received_pose) {
-            auto res =  BT::TreeNode::getInput<std::string> (item.first, value);
-            if (res){           	
-            	received_pose.at(item.first) = std::stof(value);           	
-            }
-         }
-         
-        initial_pose.position.x = received_pose["x"];
-        initial_pose.position.y = received_pose["y"];
-        initial_pose.position.z = received_pose["z"];
-        
-        tf2::Quaternion q;
-        q.setRPY(received_pose["R"],received_pose["P"],received_pose["Y"]);
-        
-        initial_pose.orientation.w = q[0];
-        initial_pose.orientation.x = q[1];
-        initial_pose.orientation.y = q[2];
-        initial_pose.orientation.z = q[3];  
-        
-        return initial_pose;
-        
-        }   */   
-
-
-using namespace BT;
 
 // SyncActionNode (synchronous action) with an input port.
 class SpawnModel : public SyncActionNode
 {
   public:
     // If your Node has ports, you must use this constructor signature 
-    SpawnModel(const std::string& name, const NodeConfiguration& config)
+    SpawnModel(const string& name, const NodeConfiguration& config)
       : SyncActionNode(name, config)
     {
        node = rclcpp::Node::make_shared("spawn_entity_client");
@@ -114,31 +81,31 @@ class SpawnModel : public SyncActionNode
     static PortsList providedPorts()
     {
         return { 
-           InputPort<std::string>("model_name"),
-           InputPort<std::string>("file_path"),
-           InputPort<std::string>("x"),
-           InputPort<std::string>("y"),
-           InputPort<std::string>("z"), 
-           InputPort<std::string>("R"),
-           InputPort<std::string>("P"),
-           InputPort<std::string>("Y"),
-           InputPort<std::string>("x_rmax"),
-           InputPort<std::string>("y_rmax"),
-           InputPort<std::string>("z_rmax"),
-           InputPort<std::string>("R_rmax"),
-           InputPort<std::string>("P_rmax"),
-           InputPort<std::string>("Y_rmax"),
+           InputPort<string>("model_name"),
+           InputPort<string>("file_path"),
+           InputPort<string>("x"),
+           InputPort<string>("y"),
+           InputPort<string>("z"), 
+           InputPort<string>("R"),
+           InputPort<string>("P"),
+           InputPort<string>("Y"),
+           InputPort<string>("x_rmax"),
+           InputPort<string>("y_rmax"),
+           InputPort<string>("z_rmax"),
+           InputPort<string>("R_rmax"),
+           InputPort<string>("P_rmax"),
+           InputPort<string>("Y_rmax"),
            };
     }
  
     
-    //Convert the received pose to initial pose. If the received pose is rand, generate a random position
+    //Function to convert the received pose to initial pose. If the received pose is rand, generate a random position
     geometry_msgs::msg::Pose convertReceivedPose(){
              
         // Form requested Pose from arguments             
         geometry_msgs::msg::Pose initial_pose;
         //Optional x,y,z, R,P,Y
-        std::map<std::string, float> received_pose;
+        map<string, float> received_pose;
         received_pose["x"] = 0;
         received_pose["y"] = 0;
         received_pose["z"] = 0;
@@ -147,17 +114,17 @@ class SpawnModel : public SyncActionNode
         received_pose["Y"] = 0;
                
         
-        std::string value;
+        string value;
         for(auto item: received_pose) {
-            if (getInput<std::string> (item.first, value)){
+            if (getInput<string> (item.first, value)){
                 //if rand is received generate a random number
                 if( value == "rand"){
 
-                   std::string max_range_str;
+                   string max_range_str;
                    int max_range;
-                   if(getInput<std::string> ( (item.first + "_rmax"), max_range_str)){
-                     //std::cout<<"max range found!!!"<<item.first<<std::endl;
-                     max_range = std::stoi(max_range_str);
+                   if(getInput<string> ( (item.first + "_rmax"), max_range_str)){
+                     //cout<<"max range found!!!"<<item.first<<endl;
+                     max_range = stoi(max_range_str);
                    }else{
                    max_range= MAX_RANGE;
                    }
@@ -169,7 +136,7 @@ class SpawnModel : public SyncActionNode
                  
                 //if a real value is received use it 
                 }else{         	
-            	received_pose.at(item.first) = std::stof(value);
+            	received_pose.at(item.first) = stof(value);
             	}           	
             }
          }
@@ -190,9 +157,9 @@ class SpawnModel : public SyncActionNode
         }
         
         //Function to append a number to the model name in case it already exists
-        std::string updateModelName(std::string model_name){
+        string updateModelName(string model_name){
         
-        auto request_model_list = std::make_shared<gazebo_msgs::srv::GetModelList::Request>();
+        auto request_model_list = make_shared<gazebo_msgs::srv::GetModelList::Request>();
         auto result_model_list = client_model->async_send_request(request_model_list);
         
          // Wait for the result.
@@ -202,12 +169,12 @@ class SpawnModel : public SyncActionNode
              auto model_names = result_model_list.get()->model_names;
              
              int model_pcs=1;
-             std::string orig_model_name = model_name;
+             string orig_model_name = model_name;
              for(int i= 0; i<int(model_names.size()); i++){
                  //if the name already exists append a number to it and start the loop again with the new name
                  if(model_names[i] == model_name){
-                     //std::cout<<model_names[i]<<" "<< model_name<<std::endl;
-                     model_name = orig_model_name + std::to_string(model_pcs); 
+                     //cout<<model_names[i]<<" "<< model_name<<endl;
+                     model_name = orig_model_name + to_string(model_pcs); 
                      model_pcs++;
                      i = 0;
                  }
@@ -221,20 +188,23 @@ class SpawnModel : public SyncActionNode
         }
      
      
+     
+     
+     
     // Override the virtual function tick()
     virtual NodeStatus tick() override
     {
-        std::string model_name;
-        std::string file_path;
+        string model_name;
+        string file_path;
        
         
         //Check whether the required inputs are provided. If not throw an error
-        if (!getInput<std::string> ("model_name", model_name))
+        if (!getInput<string> ("model_name", model_name))
         {
             throw BT::RuntimeError("missing required input [model_name]");
         }
         
-        if (!getInput<std::string> ("file_path", file_path))
+        if (!getInput<string> ("file_path", file_path))
         {
             throw BT::RuntimeError("missing required input [file_path]");
         }
@@ -242,7 +212,7 @@ class SpawnModel : public SyncActionNode
         
         
         // Waiting for spawn service /save
-        while (!client_spawn->wait_for_service(std::chrono::seconds(1))) {
+        while (!client_spawn->wait_for_service(chrono::seconds(1))) {
             if (!rclcpp::ok()) {
                 RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for service /spawn_entity.");
                 return BT::NodeStatus::FAILURE;
@@ -251,7 +221,7 @@ class SpawnModel : public SyncActionNode
         }
         
         // Waiting for model list service /save
-        while (!client_model->wait_for_service(std::chrono::seconds(1))) {
+        while (!client_model->wait_for_service(chrono::seconds(1))) {
             if (!rclcpp::ok()) {
                 RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for service /get_model_list.");
                 return BT::NodeStatus::FAILURE;
@@ -262,7 +232,7 @@ class SpawnModel : public SyncActionNode
         
         
         //Read file
-        std:: string xml_desc = readFile(node, file_path);
+         string xml_desc = readFile(node, file_path);
         
         //Calculate the initial pose based on the received values        
         geometry_msgs::msg::Pose initial_pose = convertReceivedPose();
@@ -273,7 +243,7 @@ class SpawnModel : public SyncActionNode
         
         
         //Send the request to the SpawnEntity service
-        auto request = std::make_shared<gazebo_msgs::srv::SpawnEntity::Request>();
+        auto request = make_shared<gazebo_msgs::srv::SpawnEntity::Request>();
         request->name = model_name;
         request->xml = xml_desc;
         request->initial_pose = initial_pose;
@@ -288,12 +258,12 @@ class SpawnModel : public SyncActionNode
          RCLCPP_INFO(node->get_logger(), "Waiting for result of spawning an entity...");
               
         // print some messages for feedback
-        std::cout << "[ Spawn model: " << model_name << " spawned ]" <<std::endl;
+        cout << "[ Spawn model: " << model_name << " spawned ]" <<endl;
         return NodeStatus::SUCCESS;
     }
     
     private:
-        std::shared_ptr<rclcpp::Node> node;
+        shared_ptr<rclcpp::Node> node;
         rclcpp::Client<gazebo_msgs::srv::SpawnEntity>::SharedPtr client_spawn;
         rclcpp::Client<gazebo_msgs::srv::GetModelList>::SharedPtr client_model;
 
